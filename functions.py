c68526b09a1a834cfd34ffbbce345807ca8f7c95
reinhard <buyabor@gmail.com>
2024-10-16 09:35:45 +0300

    Milestone 1

--- functions.py
+++ functions.py
@@ -8,12 +8,5 @@ def create_surface(width, height, background_color):
 
     return surface, context
 
-def draw_line(context, x1, y1, x2, y2, color, width):
-    context.set_source_rgb(*color)
-    context.set_line_width(width)
-    context.move_to(x1, y1)
-    context.line_to(x2, y2)
-    context.stroke()
-
-def write_to_surface(surface, context, filename):
+def write_to_surface(surface, filename):
     surface.write_to_png(filename)
--- main.py
+++ main.py
@@ -4,4 +4,6 @@ if __name__ == '__main__':
     # print('Hello, World!')
     surface, context = create_surface(600, 400, (0.8, 0.8, 0.8))
 
-    write_to_surface(surface, context, 'output.png')
+    
+
+    write_to_surface(surface, 'output.png')
