be74b3da304cd4b55f87a451a1e01347c5ee208f
reinhard <buyabor@gmail.com>
2024-10-16 09:43:47 +0300

    Half the house

--- functions.py
+++ functions.py
@@ -8,5 +8,17 @@ def create_surface(width, height, background_color):
 
     return surface, context
 
+def house(context, color, line_width):
+    context.set_source_rgb(*color)
+    context.set_line_width(line_width)
+    context.move_to(100, 100)
+    context.line_to(100, 150)
+    context.line_to(150, 150)
+    context.line_to(150, 300)
+    context.line_to(450, 300)
+    context.line_to(450, 150)
+    context.stroke()
+    
+
 def write_to_surface(surface, filename):
     surface.write_to_png(filename)
--- main.py
+++ main.py
@@ -4,6 +4,6 @@ if __name__ == '__main__':
     # print('Hello, World!')
     surface, context = create_surface(600, 400, (0.8, 0.8, 0.8))
 
-    
+    house(context, (0, 0, 1), 2)
 
     write_to_surface(surface, 'output.png')
