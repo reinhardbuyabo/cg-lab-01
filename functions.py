818f2da6a452ea63a0980fed7ed75a8e3d0d16c8
reinhard <buyabor@gmail.com>
2024-10-16 09:18:30 +0300

    Surface Done

--- functions.py
+++ functions.py
@@ -4,6 +4,7 @@ def create_surface(width, height, background_color):
     surface = cairo.ImageSurface(cairo.FORMAT_RGB24, width, height)
     context = cairo.Context(surface)
     context.set_source_rgb(*background_color)
+    context.paint()
 
     return surface, context
 
--- main.py
+++ main.py
@@ -4,4 +4,4 @@ if __name__ == '__main__':
     # print('Hello, World!')
     surface, context = create_surface(600, 400, (0.8, 0.8, 0.8))
 
-    surface.write_to_png('output.png')
+    write_to_surface(surface, context, 'output.png')
